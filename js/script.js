c94b566a75e08c1b7b40424f5b9d11ce39de8c49
Tachi <zaveruha007@gmail.com>
2015-06-08 18:14:12 +0300

    stuff

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var action = "click";
 var speed = 500;
 
 $(document).ready(function(){
-	$('.li.q').on(action,function(){
+	$('li.q').on(action,function(){
 		$(this).next().slideToggle(speed);
 	});
 });
