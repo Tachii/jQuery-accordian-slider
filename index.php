3db4a0e86df5bcadf4f7e9969fc65870f3030645
Tachi <zaveruha007@gmail.com>
2015-06-08 16:55:23 +0300

    stuff

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+    	<link rel="css/style.css" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>New Web Project</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     </head>
     <body>
         <h1>New Web Project Page</h1>
