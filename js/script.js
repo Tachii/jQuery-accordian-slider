8e954ee123e14b8f10138f07bd22b85b08d4dcb4
Tachi <zaveruha007@gmail.com>
2015-06-08 18:25:52 +0300

    stuff

--- js/script.js
+++ js/script.js
@@ -6,9 +6,13 @@ var speed = 500;
 
 $(document).ready(function(){
 	$('li.q').on(action,function(){
+		//Get Next Element
 		$(this)
 			.next().slideToggle(speed)
+			//Sellect All Elements
 			.siblings('li.a').slideUp();
+		//Remove Rotate Class from all incative icons, and leave only on active
 		var img = $(this).children('img');
+		
 	});
 });
